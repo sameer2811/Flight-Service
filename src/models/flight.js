641cce8dfe1fa7f50f1abb797c8e64677f1840ce
Sameer Singh <sameersingh@playsimple.in>
2024-07-30 08:53:57 +0530

    Added flight migration

---
+++ src/models/flight.js
@@ -0,0 +1,83 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Flight extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.Airplane,{
+        foreignKey:  'airplaneId',
+        onDelete : 'CASCADE'
+      });
+      this.belongsTo(models.Airport,{
+        foreignKey:  'departureAirportId',
+        onDelete : 'CASCADE'
+      });
+      this.belongsTo(models.Airport,{
+        foreignKey:  'arrivalAirportId',
+        onDelete : 'CASCADE'
+      });
+    }
+  }
+  Flight.init({
+    flightNumber: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    airplaneId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Airplanes',
+        key: 'id'
+      },
+      onDelete: 'CASCADE'
+    },
+    departureAirportId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'Airports',
+        key: 'code'
+      },
+      onDelete: 'CASCADE'
+    },
+    arrivalAirportId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'Airports',
+        key: 'code'
+      },
+      onDelete: 'CASCADE'
+    },
+    arrivalTime: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    departureTime: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    boardingGate: {
+      type: DataTypes.STRING
+    },
+    totalSeats: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Flight',
+  });
+  return Flight;
+};
